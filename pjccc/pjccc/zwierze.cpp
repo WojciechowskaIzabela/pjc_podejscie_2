e8dbab1870d12cdbfb4e44bf1905d90f30f6d90f
WojciechowskaIzabela <64603293+WojciechowskaIzabela@users.noreply.github.com>
2020-05-23 16:30:24 +0200

    Add files via upload

---
+++ pjccc/pjccc/zwierze.cpp
@@ -0,0 +1,22 @@
+#include "zwierze.h"
+
+
+
+
+char CZwierze::sprawdz_plec(CZwierze *zwierze){
+    int k='k';
+    int m='m';
+    if(zwierze->plec==k)
+        return k;
+
+    else
+        return m;
+}
+
+
+int CZwierze::sprawdz_wiek(CZwierze *zwierze){
+
+    int x=(zwierze->wiek);
+    return x;
+
+}
