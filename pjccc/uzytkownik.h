fe33087254bda7beddd4b3d136e3cf844aba5646
WojciechowskaIzabela <64603293+WojciechowskaIzabela@users.noreply.github.com>
2020-05-29 14:53:24 +0200

    pjcc29.05

---
+++ pjccc/uzytkownik.h
@@ -0,0 +1,15 @@
+#ifndef UZYTKOWNIK_H
+#define UZYTKOWNIK_H
+#include "tura.h"
+
+
+class CUzytkownik{
+
+public:
+    CUzytkownik ();
+
+    int czy_przerwac_symulacje();
+    int czy_dodac_zwierze();
+};
+
+#endif // UZYTKOWNIK_H
